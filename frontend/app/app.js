aa79782ec4f5a76da0417d5fc4cc2da812530a74
hans0126 <hayashi0126@gmail.com>
2016-10-04 17:32:33 +0800

    add lightbox

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,15 +1,25 @@
 var app = angular.module('app', ['datas']);
 
-app.controller("base", ["data_schema","toServer", function(data_schema,toServer) {
+app.controller("base", ["data_schema", "toServer", function(data_schema, toServer) {
 
     var _self = this;
 
     //_self.groupInput = "A";
 
+    _self.msg = {
+        del_group: "did you want to delete group ?",
+         del_unit: "did you want to delete unit ?",
+         del_lang: "did you want to delete language ?"
+    }
+
     _self.groups = [];
     _self.currentGroup = null;
     _self.currentUnit = null;
     _self.langs = ["en", "jp"];
+
+    _self.editMode = false;
+
+    _self.flagBoxTemp = false;
 
 
     _self.addGroup = function() {
@@ -29,13 +39,48 @@ app.controller("base", ["data_schema","toServer", function(data_schema,toServer)
 
     }
 
+    _self.changeEditMode = function() {
+        _self.editMode = !_self.editMode;
+    }
+
 
     _self.selectGroup = function(val) {
         _self.currentGroup = val;
     }
 
+
+    _self.deleteGroup = function(val) {
+        var _idx = _self.groups.indexOf(val);
+        _self.groups.splice(_idx, 1);
+        _self.currentGroup = '';
+    }
+
     _self.selectUnit = function(val) {
         _self.currentUnit = val;
+    }
+
+    _self.deleteUnit = function(val) {
+        var _idx = _self.groups.indexOf(_self.currentGroup);
+        var _unitIdx = _self.groups[_idx].units.indexOf(val);
+        _self.groups[_idx].units.splice(_unitIdx, 1);
+        _self.currentUnit = '';
+    }
+
+    _self.triggerLightbox = function() {
+
+
+    }
+
+
+
+    _self.flagBox = function(msg, fn, va) {
+
+        _self.flagBoxTemp = {
+            fn: fn,
+            va: va,
+            msg: msg
+        }
+
     }
 
     _self.addLang = function() {
@@ -113,24 +158,24 @@ app.controller("base", ["data_schema","toServer", function(data_schema,toServer)
 
     }
 
-    _self.testSend = function(){
+    _self.testSend = function() {
 
-    	toServer.saveData( _self.groups,_self.langs).success(function(){
-    		console.log("A");
-    	});
+        toServer.saveData(_self.groups, _self.langs).success(function() {
+            console.log("A");
+        });
     }
 
 }]);
 
-app.factory("toServer",["$http",function($http){
-	return {
-		saveData:function(groups,langs){
-			 return $http.post('/saveData',{
-			 	groups:groups,
-			 	langs:langs
-			 });
-		}
-	}
+app.factory("toServer", ["$http", function($http) {
+    return {
+        saveData: function(groups, langs) {
+            return $http.post('/saveData', {
+                groups: groups,
+                langs: langs
+            });
+        }
+    }
 
 }])
 
@@ -150,6 +195,88 @@ app.directive('myEnter', function() {
     };
 });
 
+
+app.directive('myEditButton', function() {
+    return {
+        restrict: "A",
+        template: "{{mode_text}}",
+        scope: {
+            mode: "=myEditButton"
+        },
+        link: function(scope, element, attrs) {
+            scope.mode_text = "edit mode";
+            element.bind('click', function() {
+                scope.mode = !scope.mode;
+                if (scope.mode) {
+                    scope.mode_text = "normal mode";
+                } else {
+                    scope.mode_text = "edit mode";
+                }
+
+                scope.$apply();
+            })
+        }
+    }
+});
+
+
+app.directive('lightBox', ['$compile', function($compile) {
+    return {
+        restrict: "A",
+
+        scope: {
+            process: "=lightBox"
+        },
+        link: function(scope, element, attrs) {
+
+            var cancelText = "cancel";
+            var outputContent = "<h1>" + scope.process.msg + "</h1>";
+            if (typeof(scope.process.fn) == "function") {
+                outputContent += "<button ng-click='cancel()'>Cancel</button><button ng-click='delete()'>Delete</button>";
+            } else {
+                outputContent += "<button ng-click='cancel()'>Ok</button>";
+            }          
+
+            scope.cancel = function() {
+                scope.process = "";
+            }
+
+            scope.delete = function() {
+
+                if (Object.prototype.toString.call(scope.process.va) === '[object Array]') {
+                    scope.process.fn.apply(null, scope.process.va);
+                } else {
+                    scope.process.fn(scope.process.va);
+                }
+
+                scope.process = "";
+            }
+
+            var el = angular.element(outputContent);
+            element.append(el);
+            $compile(el)(scope);
+
+        }
+    }
+}])
+
+
+app.filter('sprintf', function() {
+
+    function parse(str) {
+        var args = [].slice.call(arguments, 1),
+            i = 0;
+
+        return str.replace(/%s/g, function() {
+            return args[i++];
+        });
+    }
+
+    return function(str) {
+        return parse(str, arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
+    };
+
+})
 
 var datas = angular.module('datas', []);
 
